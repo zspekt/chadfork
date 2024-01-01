8970d89fa09b34b8c4abfe78f88fc0a0b2c1f73f
speki <zspekt@tutanota.com>
2024-01-01 15:31:20 -0300

    treesitter for hyprland config files
    Signed-off-by: speki <zspekt@tutanota.com>

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,5 +1,16 @@
 local options = {
   ensure_installed = { "lua" },
+  parsers = function()
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.hypr = {
+      install_info = {
+        url = "https://github.com/luckasRanarison/tree-sitter-hypr",
+        files = { "src/parser.c" },
+        branch = "master",
+      },
+      filetype = "hypr",
+    }
+  end,
 
   highlight = {
     enable = true,
